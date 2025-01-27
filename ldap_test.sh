ba3fda8f2f9d21cb1427c3e2ffb0117940da2ddb
AmaRocket <alexeverdie999@gmail.com>
2025-01-27 15:28:16 +0100

    remove sh script

--- ldap_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LDAP_SERVER="ldap://unibasel.ads.unibas.ch"
-BIND_DN="d-dmi-admgmt@unibasel.ads.unibas.ch"
-BASE_DN="DC=unibasel,DC=ads,DC=unibas,DC=ch"
-
-read -p "Enter username or email to search: " USER_INPUT
-ldapsearch -x -b "$BASE_DN" -W -D "$BIND_DN" -H "$LDAP_SERVER" -ZZ \
-    "(|(uid=$USER_INPUT)(mail=$USER_INPUT))" cn mail sAMAccountName
